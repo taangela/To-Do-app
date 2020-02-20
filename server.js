0e245bc7d143be61f10ba6d3e6d68dfb937d9f17
Marta Koperek <makoperek@gmail.com>
2020-02-21 00:02:04 +0100

    initiate project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+//const path = require('path');
+//const socket = require('socket.io');
+
+const app = express();
+
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running...');
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
