85c3d07290c51bdfc588464216f2bdae1b93b9a0
Marta Koperek <makoperek@gmail.com>
2020-02-21 00:34:05 +0100

    Add listeners and emmitters

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+//import io from 'socket.io-client';
+
+//const socket = io();
 
 class App extends React.Component {
 
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
 //const path = require('path');
-//const socket = require('socket.io');
+const socket = require('socket.io');
 
 const app = express();
 
+const tasks =[];
 
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+  console.log('Server is running on port 8000');
+});
+
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    console.log('New task added ' + task);
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('rmoveTask', (tasksIndex) => {
+    console.log('Task with index ' + taskIndex + ' removed');
+    tasks.splice(taskIndex, 1);
+    socket.broadcast.emit('removeTask');
+  });
 });
 
 app.use((req, res) => {
